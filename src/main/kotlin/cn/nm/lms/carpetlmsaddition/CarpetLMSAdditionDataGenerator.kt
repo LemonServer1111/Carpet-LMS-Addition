4f1b7431c3f6bdfaa0ec46725c9ce63eb63dec68
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-12-16 01:09:35 +0800

    Remove DataGen

--- src/main/kotlin/cn/nm/lms/carpetlmsaddition/CarpetLMSAdditionDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package cn.nm.lms.carpetlmsaddition
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object CarpetLMSAdditionDataGenerator : DataGeneratorEntrypoint {
-    override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-    }
-}
